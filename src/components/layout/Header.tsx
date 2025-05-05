3de82ff05f07d30fa6d20d512b7930dbd54ad8f2
engelhardniels <engelhardniels@gmail.com>
2025-05-05 22:26:13 +0200

    some header changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,16 +7,25 @@ export default function Header() {
     return (
       <header className="w-full layout-border layout-border-bottom flex justify-center">
         <div className="max-w-7xl flex justify-between w-full p-4 layout-border layout-border-left layout-border-right">
-          <div className="text-xl font-bold flex gap-1 items-center">
-            <Link href="/"><div>Letter-League</div></Link>
+
+          <div>
+            <div className="text-xl font-bold flex gap-1 items-center leading-none">
+              <Link href="/"><div>Letter-League</div></Link>
+            </div>
+            <div className="text-xs w-full text-center">
+              &lt;username&gt;
+            </div>            
           </div>
+
           <div>
             <TextButton text="Play Game" href="/play" />
           </div>
+
           <div className="flex gap-2">
             <IconButton icon={Settings} label="Settings" href="/settings"></IconButton>
             <IconButton icon={CircleUserRound} label="Account" href="/account"></IconButton>
           </div>
+
         </div>
       </header>
     );

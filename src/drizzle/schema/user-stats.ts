a8f7a0c5e935c55efed8f88bb06e458e20f00863
Niels Engelhard <niels@robinhq.com>
2025-05-15 16:49:41 +0200

    WIP

--- src/drizzle/schema/user-stats.ts
+++ src/drizzle/schema/user-stats.ts
@@ -1,15 +1,16 @@
-import { numeric, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { integer, json, numeric, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { UsersTable } from "./users";
 import { relations } from "drizzle-orm";
 
 export const UserStatsTable = pgTable("user_stats", {
     id: uuid().primaryKey().references(() => UsersTable.id, { onDelete: "cascade" }),
-    soloGamesPlayed: numeric().notNull(),
-    multiplayerGamesPlayed: numeric().notNull(),
-    wordOfTheDaysPlayed: numeric().notNull(),
-    wordOfTheDayWins: numeric().notNull(),    
+    soloGamesPlayed: integer().notNull().default(0),
+    multiplayerGamesPlayed: integer().notNull().default(0),
+    wordOfTheDaysPlayed: integer().notNull().default(0),
+    wordOfTheDayWins: integer().notNull().default(0),    
     lastGamePlayed: timestamp(),
-  });
+    badges: json<string[]>().notNull().default([]),
+  });``
 
 export const UserStatsRelations = relations(UserStatsTable, ({ one }) => ({
     user: one(UsersTable, {
